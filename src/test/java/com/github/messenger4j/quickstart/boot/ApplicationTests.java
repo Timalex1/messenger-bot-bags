4313017606570ce0e0c202ad15a2d40025f3c0ab
timalex1 <timilehinvincent@gmail.com>
2022-07-01 03:08:42 +0100

    ...testing it out

---
+++ src/test/java/com/github/messenger4j/quickstart/boot/ApplicationTests.java
@@ -0,0 +1,14 @@
+package com.github.messenger4j.quickstart.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+public class ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
